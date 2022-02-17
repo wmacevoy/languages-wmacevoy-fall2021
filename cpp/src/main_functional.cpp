5234cfc5e43a42b4725a9227ba2d9f72f0661f8e
Warren MacEvoy <wmacevoy@gmail.com>
2022-02-17 09:47:57 -0700

    functional example

--- cpp/src/main_functional.cpp
+++ cpp/src/main_functional.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <iostream>
 
 using namespace std;
 class WebTemplate {
    map<string,string> dictionary; // dictionary["who"]="bob"
    string component;  /// ex <p>hello ${who}</p>
    string translate() const {
-// replace all the ${name} with value in dictionary in component
-// return that: ex <p>hello bob</p>
+       return "";
    }
 };
 
@@ -25,7 +25,7 @@ const string p(const string &text) {
     return "<p>" + text  + "</p>";
 }
 
-string ps(const vector<const string> &texts, int from = 0) {
+string ps(const vector<string> &texts, int from = 0) {
    if (texts.size() <= from) return "";
    else return texts[from] + ps(texts,from+1);  
    
@@ -39,13 +39,12 @@ string ps_nonfunctional_impl(vector<string> texts) {
     // philosphically bad -- modifying a value...
     string ans = "";
     for (auto text : texts) {
-        wait for RLIMIT_DATA
-        do with data
         ans += p(text);
     }
     return ans;
 }
 
 int main() {
+    cout << p("hello world") << endl;
    //  page(header(),title(),body(vector([p("hi)"),p("there")]))));
 }
